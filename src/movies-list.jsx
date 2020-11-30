920a9f038391c01280b6567a02ef21ad36b9c28e
Mark Tripney <mrktrpny@gmail.com>
2020-11-30 12:28:19 +0100

    Add filter option

--- src/movies-list.jsx
+++ src/movies-list.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const movies = [
+  { name: 'The Grand Budapest Hotel', year: '2014' },
+  { name: 'The Royal Tenenbaums', year: '2001' },
+  { name: 'The Life Aquatic with Steve Zissou', year: '2004' },
+  { name: 'Moonrise Kingdom', year: '2012' },
+  { name: 'Rushmore', year: '1998' },
+];
 
 export function MoviesList() {
-  const movies = [
-    { name: 'The Grand Budapest Hotel' },
-    { name: 'The Royal Tenenbaums' },
-    { name: 'The Life Aquatic with Steve Zissou' },
-    { name: 'Rushmore' },
-    { name: 'Moonrise Kingdom' },
-  ];
+  const [filter, setFilter] = useState('');
   return (
     <div>
+      <h1>(Some) films of Wes Anderson</h1>
+      <label>
+        Filter:
+        <input
+          type="text"
+          onChange={e => setFilter(e.target.value)}
+          value={filter}
+        />
+      </label>
       <ul>
-        {movies.map(movie => {
-          return <li key={movie.name}>{movie.name}</li>;
-        })}
+        {movies
+          .filter(movie =>
+            movie.name.toLowerCase().includes(filter.toLowerCase())
+          )
+          .map(movie => (
+            <li key={movie.name}>
+              {movie.name} ({movie.year})
+            </li>
+          ))}
       </ul>
     </div>
   );
